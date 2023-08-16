451690b4ae1d140890710076dc553368f7ddad36
bershath <bershath@gmail.com>
2023-08-16 14:00:33 +1000

    Initial commit

---
+++ eap-delivery-active-desc/src/main/java/org/jboss/labs/eap/DelActiveDescMDB.java
@@ -0,0 +1,35 @@
+package org.jboss.labs.eap;
+
+import org.jboss.logging.Logger;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+@MessageDriven(
+        activationConfig = {
+                @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
+                @ActivationConfigProperty(propertyName = "destination", propertyValue = "/jms/queue/A"),
+                @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge"),
+                @ActivationConfigProperty(propertyName = "user", propertyValue = "admin"),
+                @ActivationConfigProperty(propertyName = "password", propertyValue = "jboss10)")
+        })
+@TransactionManagement(TransactionManagementType.CONTAINER)
+public class DelActiveDescMDB implements MessageListener {
+    private static Logger log = Logger.getLogger(DelActiveDescMDB.class);
+    @Override
+    public void onMessage(Message message) {
+        if (message instanceof TextMessage){
+            try {
+                log.info(message.getBody(String.class));
+            } catch (JMSException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
